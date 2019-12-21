cec626f84c2f1fa4c82d39cd34b33e7741f4e88f
warkoczek <53393030+warkoczek@users.noreply.github.com>
2019-12-21 09:01:36 +0100

    App main test - find book by title

--- src/main/java/awarkoczewski/App.java
+++ src/main/java/awarkoczewski/App.java
@@ -1,11 +1,18 @@
 package awarkoczewski;
 
 
-public class App 
+import awarkoczewski.books.BooksFacade;
+
+import awarkoczewski.books.model.Book;
+import awarkoczewski.books.repository.InMemoryBookRepository;
+
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
+        BooksFacade booksFacade = new BooksFacade(new InMemoryBookRepository());
 
+        Book book = booksFacade.getBookByTitle("Flights");
 
+        System.out.println(book.toString());
     }
 }
